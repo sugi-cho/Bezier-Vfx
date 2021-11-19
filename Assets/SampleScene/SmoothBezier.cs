b8b363aeed69146d63c21dc414ccc0933395eadc
sugino <git@sugi.cc>
2021-11-19 17:40:34 +0900

    add 2d Bezier mode

--- Assets/SampleScene/SmoothBezier.cs
+++ Assets/SampleScene/SmoothBezier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using sugi.vfx.bezier;
-using BezierSegment = sugi.vfx.bezier.BezierVfx.BezierSegment;
+using BezierSegment3D = sugi.vfx.bezier.BezierVfx.BezierSegment3D;
 
 [ExecuteInEditMode]
 public class SmoothBezier : MonoBehaviour
@@ -13,7 +13,7 @@ public class SmoothBezier : MonoBehaviour
     [SerializeField] BezierVfx bezierVfx;
 
     bool edit;
-    List<BezierSegment> m_bezierSegments = new List<BezierSegment>();
+    List<BezierSegment3D> m_bezierSegments = new List<BezierSegment3D>();
 
     private void OnValidate()
     {
@@ -22,7 +22,7 @@ public class SmoothBezier : MonoBehaviour
         {
             var s0 = smoothBeziers[i];
             var s1 = smoothBeziers[(i + 1) % smoothBeziers.Length];
-            var segment = new BezierSegment { p0 = s0.p0, p1 = s0.p1, p2 = 2 * s1.p0 - s1.p1, p3 = s1.p0 };
+            var segment = new BezierSegment3D { p0 = s0.p0, p1 = s0.p1, p2 = 2 * s1.p0 - s1.p1, p3 = s1.p0 };
             m_bezierSegments.Add(segment);
         }
         edit = true;
@@ -31,7 +31,7 @@ public class SmoothBezier : MonoBehaviour
     private void Update()
     {
         if (edit)
-            bezierVfx.BezierSegments = m_bezierSegments;
+            bezierVfx.BezierSegments3D = m_bezierSegments;
         edit = false;
     }
 
--- Packages/BezierVfx/BezierVfx.cs
+++ Packages/BezierVfx/BezierVfx.cs
@@ -13,28 +13,51 @@ namespace sugi.vfx.bezier
         [SerializeField] string bufferPropName = "BezierBuffer";
         [SerializeField] string numPropName = "nSegments";
         [SerializeField] int maxBezierSegments = 512;
+        [SerializeField] SegmentType segmentType = SegmentType._2D;
 
         GraphicsBuffer m_buffer;
-        List<BezierSegment> m_bezierSegments;
+        List<BezierSegment2D> m_bezierSegments2d;
+        List<BezierSegment3D> m_bezierSegments3d;
 
-        public List<BezierSegment> BezierSegments
+        int numDimensions => (int)segmentType;
+
+        public List<BezierSegment2D> BezierSegments2D
         {
-            get => m_bezierSegments;
+            get => m_bezierSegments2d;
             set
             {
-                m_bezierSegments = value;
-                UpdateVfx();
+                if (segmentType == SegmentType._2D)
+                {
+                    m_bezierSegments2d = value;
+                    UpdateVfx();
+                }
+                else
+                    Debug.LogWarning($"segmentType({segmentType}) is not match {SegmentType._2D}");
+            }
+        }
+        public List<BezierSegment3D> BezierSegments3D
+        {
+            get => m_bezierSegments3d;
+            set
+            {
+                if (segmentType == SegmentType._3D)
+                {
+                    m_bezierSegments3d = value;
+                    UpdateVfx();
+                }
+                else
+                    Debug.LogWarning($"segmentType({segmentType}) is not match {SegmentType._3D}");
             }
         }
         /// <summary>
-        /// SetData for Unity.Vectorgraphics.BezierSegment
+        /// Set Data Array Directory
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="data"></param>
         public void SetData<T>(T[] data)
         {
             if (m_buffer == null)
-                m_buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4 * maxBezierSegments, sizeof(float) * 3);
+                m_buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4 * maxBezierSegments, sizeof(float) * numDimensions);
             m_buffer.SetData(data);
             vfx.SetGraphicsBuffer(bufferPropName, m_buffer);
             vfx.SetInt(numPropName, data.Length);
@@ -42,34 +65,62 @@ namespace sugi.vfx.bezier
 
         private void OnEnable()
         {
-            m_buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4 * maxBezierSegments, sizeof(float) * 3);
+            m_buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4 * maxBezierSegments, sizeof(float) * numDimensions);
             vfx.SetGraphicsBuffer(bufferPropName, m_buffer);
         }
 
 #if UNITY_EDITOR
         private void OnDrawGizmosSelected()
         {
-            m_bezierSegments.ForEach(s
+            m_bezierSegments3d.ForEach(s
                => Handles.DrawBezier(s.p0, s.p3, s.p1, s.p2, Color.blue, null, 2f));
+        }
+        private void OnValidate()
+        {
+            if (m_buffer != null)
+                m_buffer.Release();
+            m_buffer = null;
         }
 #endif
 
         void UpdateVfx()
         {
             if (m_buffer == null)
-                m_buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4 * maxBezierSegments, sizeof(float) * 3);
-            m_buffer.SetData(m_bezierSegments);
+                m_buffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 4 * maxBezierSegments, sizeof(float) * numDimensions);
+            switch (segmentType)
+            {
+                case SegmentType._2D:
+                    m_buffer.SetData(m_bezierSegments2d);
+                    break;
+                case SegmentType._3D:
+                    m_buffer.SetData(m_bezierSegments3d);
+                    break;
+            }
             vfx.SetGraphicsBuffer(bufferPropName, m_buffer);
-            vfx.SetInt(numPropName, m_bezierSegments.Count);
+            vfx.SetInt(numPropName, segmentType == SegmentType._2D ? m_bezierSegments2d.Count : m_bezierSegments3d.Count);
         }
 
         [System.Serializable]
-        public struct BezierSegment
+        public struct BezierSegment2D
+        {
+            public Vector2 p0;
+            public Vector2 p1;
+            public Vector2 p2;
+            public Vector2 p3;
+        }
+        [System.Serializable]
+        public struct BezierSegment3D
         {
             public Vector3 p0;
             public Vector3 p1;
             public Vector3 p2;
             public Vector3 p3;
+        }
+
+        public enum SegmentType
+        {
+            _2D = 2,
+            _3D = 3,
         }
     }
 }
